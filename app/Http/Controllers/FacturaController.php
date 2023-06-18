853747f9880a7bef3d0cd635e811c6f3475d2136
Kevin Sierra <kevinfsierra43@gmail.com>
2023-06-18 08:14:59 -0500

    feat: Inicialización del módulo

---
+++ app/Http/Controllers/FacturaController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Factura;
+use Illuminate\Support\Facades\DB;
+
+class FacturaController extends Controller
+{
+
+    public function getAll()
+    {
+        $facturas = Factura::select(
+            'factura.num_factura',
+            'cliente.cedula AS cliente_cedula',
+            DB::raw("CONCAT(cliente.nombre, ' ', cliente.apellido) AS cliente_nombre"),
+            'factura.created_at AS fecha'
+        )
+        ->join('cliente', 'cliente.id_cliente', '=', 'factura.id_cliente')
+        ->get();
+
+        return $facturas;
+    }
+
+}
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,11 @@
 <template>
-    <div>
-        Inicialización
+    <div class="bg-slate-300 min-w-screen min-h-screen px-16 py-8">
+        <ViewFactura/>
     </div>
 </template>
+
+<script setup>
+
+import ViewFactura from '@/components/ViewFactura.vue';
+
+</script>
