70bf8e8a35713376c414e645fffdd2ed781621de
Kevin Sierra <kevinfsierra43@gmail.com>
2023-06-19 16:47:25 -0500

    change: tailwind cdn por tailwind cli

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -4,9 +4,11 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Laravel</title>
-        <script src="https://cdn.tailwindcss.com"></script>
+
+        <link rel="stylesheet" href="{{ asset('build/assets/output.css') }}">
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+
 
         <style>
 
